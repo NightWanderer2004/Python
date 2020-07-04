31cbb4d9459541d6815c9c209b1acfec5a1a02ee
NightWanderer2004 <67825832+NightWanderer2004@users.noreply.github.com>
2020-07-04 22:59:06 +0300

    Fun

---
+++ tritritri/tritritri/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tritritri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter numbers:");
+            int height = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    for (int i = 0; i <= j; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (int i = height; i > j; i--)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+                for (int x = 0; x < height; x++)
+                {
+                    for (int z = 0; z <= x; z++)
+                    {
+                        Console.Write("#");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
